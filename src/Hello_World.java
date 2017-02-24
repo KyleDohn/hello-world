7e3a41d6a2be3b25024dcb6a9a6815fa5a1325d2
kyl3t <kyl3t@DESKTOP-9UKH27Q.home>
2017-02-23 20:54:59 -0500

    added cruel

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -2,7 +2,7 @@
 public class Hello_World {
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello,  Cruel World");
     }
 
 }
